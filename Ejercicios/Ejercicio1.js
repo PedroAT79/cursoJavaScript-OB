926279af7bfdcfe37b990536171aa10285a15985
PedroAT79 <plat1505@gmail.com>
2022-10-06 23:39:58 +0200

    Ejercicio1

---
+++ Ejercicios/Ejercicio1.js
@@ -0,0 +1,10 @@
+const miNombre = Pedro;
+const miEdad = 43;
+let isDevelop = true;
+const miFechaNacimient = new Date(1979,3,17); // 17/04/1979
+const miLibroFavorito = {
+    titulo: "El Quijote",
+    autor: "Cervantes",
+    fecha: new Date(01,1,1615),
+    url: "https://es.wikipedia.org/wiki/Don_Quijote_de_la_Mancha"
+}
